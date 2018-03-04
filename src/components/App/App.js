44bbfc710187279e81d806a2413bd0dd09c35593
Sandun <sandunwebdev@gmail.com>
2018-03-04 06:53:56 +0530

    Added Header Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from "../Header/Header"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>RetailMeNot</h1>
+        <Header/>
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Header.css"
+
+class Header extends Component{
+    render(){
+        return (
+            <div className="headerOverlay">
+                <div className="header">
+                    Header Component
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
