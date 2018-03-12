b5f27687358ed0b75332f3db0eaa51b39f64a8f2
Sandun <sandunwebdev@gmail.com>
2018-03-12 07:49:51 +0530

    BugFix - Added trendingReducer to Store, for fix Search Component bug

--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,13 @@ import logger from "redux-logger";
 
 import searchReducer from "../reducers/searchReducer";
 import storesReducer from "../reducers/storesReducer";
+import trendingReducer from "../reducers/storesReducer";
 import defaultState from "./defaultState";
 
 const rootReducer = combineReducers({
   search: searchReducer,
-  stores: storesReducer
+  stores: storesReducer,
+  trending: trendingReducer
 });
 
 const middlewares = applyMiddleware(thunk, logger);
