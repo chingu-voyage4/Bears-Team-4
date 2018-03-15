06a1e2f0363d2ad4ea7b1cc5db91c96c4d527a41
Anshul Dubey <anshuldubey2166@gmail.com>
2018-03-15 17:59:26 +0530

    Password hide/show working

--- src/Components/Signup/RightSection/RightSection.js
+++ src/Components/Signup/RightSection/RightSection.js
@@ -25,13 +25,33 @@ import {
 } from "react-bootstrap";
 import "./RightSection.css";
 
-const RightSection = props => (
-	<MuiThemeProvider>
-		<Col sm={9}>
+
+
+
+class RightSection extends Component {
+
+constructor(props){
+    super(props);
+    this.state = {
+      type: 'password'
+    }
+    this.showHide = this.showHide.bind(this);
+  }
+showHide(e){
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({
+      type: this.state.type === 'input' ? 'password' : 'input'
+    })  
+  }
+  render(){
+  	return(
+  		<MuiThemeProvider>
+		<Col mdOffset={3} sm={9}>
 			<div className="right-section">
 				<div className="upper-card">
 					<Card>
-						<CardText>Already have an account? Log in</CardText>
+						<CardText><span className="upper-card-text">Already have an account? <a href="#">Log in</a></span></CardText>
 					</Card>
 				</div>
 				<div className="lower-card">
@@ -46,65 +66,103 @@ const RightSection = props => (
 			</div>
 		</Col>
 	</MuiThemeProvider>
-);
+  	);
+  }
+}
 
 const SignWithFb = props => (
-	<Button type="submit" bsStyle="primary" block>
-		<InputGroup>
-			<InputGroup.Button>
-				<FacebookIcon className="footer-icons" />
-			</InputGroup.Button>
-			Sign with Facebook
-		</InputGroup>
-	</Button>
-);
-
-const Divider = props => (
-	<div className="divider">
-		<span className="divider_content">OR</span>
+	<div className="section">
+		<Button type="submit" bsStyle="primary" bsSize="large" block>
+			<InputGroup>
+				<InputGroup.Button>
+					<FacebookIcon className="footer-icons" />
+				</InputGroup.Button>
+				<span className="field-text">Sign with Facebook</span>
+			</InputGroup>
+		</Button>
 	</div>
 );
 
-const FormInstance = props => (
-	<Form horizontal>
-		<FormGroup className="group-content" controlId="formHorizontalEmail">
-			<Col>
-				<InputGroup>
-					<InputGroup.Addon>
-						<Glyphicon glyph="envelope" />
-					</InputGroup.Addon>
-					<FormControl type="email" placeholder="Email" />
-				</InputGroup>
-			</Col>
-		</FormGroup>
-
-		<FormGroup className="group-content" controlId="formHorizontalPassword">
-			<Col>
-				<InputGroup>
-					<InputGroup.Addon>
-						<Glyphicon glyph="lock" />
-					</InputGroup.Addon>
-					<FormControl type="password" placeholder="Password" />
-
-					<InputGroup.Button>
-						<Button>Show</Button>
-					</InputGroup.Button>
-				</InputGroup>
-			</Col>
-		</FormGroup>
-		<FormGroup className="group-content">
-			<Col>
-				<Checkbox>Email me the best deals on RetailMeNot</Checkbox>
-			</Col>
-		</FormGroup>
-		<FormGroup className="group-content">
-			<Col>
-				<Button type="submit" bsStyle="primary" bsSize="large" block>
-					Sign in
-				</Button>
-			</Col>
-		</FormGroup>
-	</Form>
+const Divider = props => (
+	<div className="section">
+		<div className="divider">
+			<span className="divider_content">OR</span>
+		</div>
+	</div>
 );
+
+class FormInstance extends Component{
+	constructor(props){
+    super(props);
+    this.state = {
+      type: 'password'
+    }
+    this.showHide = this.showHide.bind(this);
+  }
+showHide(e){
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({
+      type: this.state.type === 'input' ? 'password' : 'input'
+    })  
+  }
+  render(){
+  	return(
+  		<div className="section">
+		<Form horizontal>
+			<FormGroup
+				className="group-content"
+				controlId="formHorizontalEmail"
+			>
+				<Col>
+					<InputGroup>
+						<InputGroup.Addon>
+							<Glyphicon glyph="envelope" />
+						</InputGroup.Addon>
+						<FormControl type="email" placeholder="Email" />
+					</InputGroup>
+				</Col>
+			</FormGroup>
+
+			<FormGroup
+				className="group-content"
+				controlId="formHorizontalPassword"
+			>
+				<Col>
+					<InputGroup>
+						<InputGroup.Addon>
+							<Glyphicon glyph="lock" />
+						</InputGroup.Addon>
+						<FormControl type={this.state.type} placeholder="Password" />
+
+						<InputGroup.Button>
+							<Button onClick={this.showHide}>{this.state.type === 'input' ? 'Hide' : 'Show'}</Button>
+						</InputGroup.Button>
+					</InputGroup>
+				</Col>
+			</FormGroup>
+			<FormGroup className="group-content">
+				<Col>
+					<Checkbox>Email me the best deals on RetailMeNot</Checkbox>
+				</Col>
+			</FormGroup>
+			<FormGroup className="group-content">
+				<Col>
+					<Button
+						type="submit"
+						bsStyle="primary"
+						bsSize="large"
+						block
+						disabled
+					>
+						Sign in
+					</Button>
+				</Col>
+			</FormGroup>
+		</Form>
+	</div>
+  	);
+  }
+}
 
 export default RightSection;
