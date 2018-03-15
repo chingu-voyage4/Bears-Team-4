d98561c730ae3f8e4ab08b4fd1436a0d888adf01
Anshul Dubey <anshuldubey2166@gmail.com>
2018-03-15 23:15:23 +0530

    Left section text styled

--- src/Components/Signup/LeftSection/LeftSection.js
+++ src/Components/Signup/LeftSection/LeftSection.js
@@ -7,7 +7,7 @@ import {
 	CardTitle,
 	CardText
 } from "material-ui/Card";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col } from "react-bootstrap";
 import Avatar from "material-ui/Avatar";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import "./LeftSection.css";
@@ -19,19 +19,19 @@ const LeftSection = props => (
 		<div className="left-section">
 			<h1>Create an account</h1>
 			<strong>It's FREE and allows you to:</strong>
-			<Services title="Track your savings—including Cash Back and Discount Gift Cards!Save the deals you want and quickly access them at any time." />
+			<Services title="Track your savings—including Cash Back and Discount Gift Cards!" />
 			<Services title="Save the deals you want and quickly access them at any time." />
 			<Services title="Get recommendations on deals from your favorite brands." />
 		</div>
 	</MuiThemeProvider>
 );
 
-const Services=props=>(
+const Services = props => (
 	<Card style={style}>
-	<CardHeader
-          title={props.title}
-          avatar="../../images/android.jpg"
-    />
-    </Card>
+		<CardHeader
+			title={<div className="left-section-title">{props.title}</div>}
+			avatar="../../images/android.jpg"
+		/>
+	</Card>
 );
 export default LeftSection;
