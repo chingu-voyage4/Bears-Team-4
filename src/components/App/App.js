a90efd6300f2e3b2612bb355a252c02d67be2754
Anshul Dubey <anshuldubey2166@gmail.com>
2018-03-16 12:32:22 +0530

    Merged login and signup Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,6 +10,7 @@ import CategoryPage from "../CategoryPage/CategoryPage";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import LoginPage from "../LoginPage/LoginPage";
+import Signup from "../Signup/Signup";
 import MainPage from "../MainPage/MainPage";
 import SearchPage from "../SearchPage/SearchPage";
 import StoresPage from "../StoresPage/StoresPage";
@@ -23,6 +24,7 @@ class App extends Component {
         <Header />
         <Route exact path="/" component={MainPage} />
         <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/signup" component={Signup} />
         <Route path="/stores/:storeName" component={StoresPage} />
         <Route path="/search/:term" component={SearchPage} />
         <Route path="/category/:categoryName" component={CategoryPage} />
