4a4d1517d993b026f73189fd21010fd6905b5e03
Anshul2166 <anshuldubey2166@gmail.com>
2018-03-22 23:28:12 +0530

    Added Left-section

---
+++ src/components/SearchPage/LeftSection/LeftSection.js
@@ -0,0 +1,78 @@
+/**
+ * This comopent render TrendingDeals + TopOffer + Populars + Sucribe.
+ *
+ * In here we connect with redux storage and get inject needed states and action as props.
+ * Then we pass relevent props to child components.
+ **/
+
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Row, Col, Image, Checkbox } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import "./LeftSection.css";
+
+class LeftSection extends Component {
+    render() {
+        const { trendingItems, coupons, stores } = this.props;
+        return (
+            <div className="left-section">
+            <ImageLogo src=""/>
+            <p>50 offers available</p>
+            <CouponType />
+            <About />
+            <Stats />
+            </div>
+        );
+    }
+}
+
+const ImageLogo = props => (
+    <div className="image-logo">
+      <Image src="/thumbnail.png" rounded />
+    </div>
+);
+
+const CouponType = props => (
+   <div className="coupon-type">
+        <h2>Coupon Type</h2>
+        <CheckBoxCoupon title="Coupon Code"/>
+        <CheckBoxCoupon title="Online Sales"/>
+   </div>
+);
+
+const CheckBoxCoupon=props=>(
+    <Checkbox checked readOnly>
+      Checkbox
+    </Checkbox>
+);
+
+const About = props => (
+   <div className="about">
+        <h2>About Udemy</h2>
+        <p className="about-content">Udemy offers a platform to take and build online courses on any subject. The course categories include academic courses, business courses, professional courses, creative and performing arts, health and fitness, language courses, lifestyle, music, technology among many others.</p>
+        <TopOffers />
+   </div>
+);
+
+const TopOffers=props=>(
+    <div className="top-offers">
+    <h2>Today's Udemy Top Offers</h2>
+    <ul>
+    <li>3 Courses Or More for $9.99</li>
+    <li>95% Off Ethical Hacking With Android Fast-Track Course</li>
+    </ul>
+    </div>
+);
+
+const Stats=props=>(
+    <div className="stats">
+        <ul>
+            <li>Total Offers:</li>
+            <li>Coupon Codes:</li>
+            <li>Best Discount:</li>
+            <li>Average Savings:</li>
+        </ul>
+    </div>
+);
+
+export default LeftSection;
--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -10,6 +10,7 @@ import { connect } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import Coupon from "../Common/Coupon/Coupon";
+import LeftSection from "./LeftSection/LeftSection";
 import "./SearchPage.css";
 
 class SearchPage extends Component {
@@ -17,32 +18,42 @@ class SearchPage extends Component {
         const { trendingItems, coupons, stores } = this.props;
         return (
             <div className="mainPage">
-                <div className="View-Page">
-                    <Row>
-                        <div className="view-title">
-                            <h1 className="text-center">Udemy Coupon Codes</h1>
-                        </div>
-                    </Row>
-                    <Row>
-                        <div className="sortBy">
-                            <Col mdOffset={1} md={3}>
-                                <SortButton title="Popularity" />
-                            </Col>
-                            <Col mdOffset={1} md={3}>
-                                <SortButton title="Newest" />
-                            </Col>
-                            <Col mdOffset={1} md={3}>
-                                <SortButton title="Ending Soon" />
-                            </Col>
-                        </div>
-                    </Row>
-                    <Row>
-                        <div className="coupon">
-                            <Col smOffset={2}>
-                                <Coupon coupon={coupons[0]} store={stores} />
-                            </Col>
-                        </div>
-                    </Row>
+                <div className="LeftSection">
+                    <LeftSection />
+                </div>
+                <div className="RightSection">
+                    <div className="View-Page">
+                        <Row>
+                            <div className="view-title">
+                                <h1 className="text-center">
+                                    Udemy Coupon Codes
+                                </h1>
+                            </div>
+                        </Row>
+                        <Row>
+                            <div className="sortBy">
+                                <Col mdOffset={1} md={3}>
+                                    <SortButton title="Popularity" />
+                                </Col>
+                                <Col mdOffset={1} md={3}>
+                                    <SortButton title="Newest" />
+                                </Col>
+                                <Col mdOffset={1} md={3}>
+                                    <SortButton title="Ending Soon" />
+                                </Col>
+                            </div>
+                        </Row>
+                        <Row>
+                            <div className="coupon">
+                                <Col smOffset={2}>
+                                    <Coupon
+                                        coupon={coupons[0]}
+                                        store={stores}
+                                    />
+                                </Col>
+                            </div>
+                        </Row>
+                    </div>
                 </div>
             </div>
         );
