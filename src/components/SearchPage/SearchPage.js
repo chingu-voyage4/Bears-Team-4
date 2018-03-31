89fcb79fa5238db47e9147309786a3a92a924b2a
Anshul2166 <anshuldubey2166@gmail.com>
2018-04-01 00:29:43 +0530

    Refracted to views page completly

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,47 +1,16 @@
-/**
- * This comopent render TrendingDeals + TopOffer + Populars + Sucribe.
- *
- * In here we connect with redux storage and get inject needed states and action as props.
- * Then we pass relevent props to child components.
- **/
-
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Row, Col } from "react-bootstrap";
-import { Button } from "react-bootstrap";
-import Coupon from "../Common/Coupon/Coupon";
-import LeftSection from "./LeftSection/LeftSection";
-import RightSection from "./RightSection/RightSection";
 import "./SearchPage.css";
 
 class SearchPage extends Component {
-    render() {
-        const { trendingItems, coupons, stores } = this.props;
-        return (
-            <div className="mainPage">
-                <Row>
-                    <Col md={3}>
-                        <div className="LeftSection">
-                            <LeftSection />
-                        </div>
-                    </Col>
-                    <Col mdOffset={1} md={8}>
-                        <RightSection coupons={coupons} stores={stores}/>
-                    </Col>
-                </Row>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="searchPage">
+        <h1 style={{ fontSize: "100px" }}>
+          Search Page - {this.props.match.params.term}
+        </h1>
+      </div>
+    );
+  }
 }
 
-// Specifying which state from store we want as props
-const mapStateToProps = state => {
-    return {
-        stores: state.stores,
-        coupons: state.coupons,
-        trendingItems: state.trending
-    };
-};
-
-// Binding stores states and actions to our compoent's props
-export default connect(mapStateToProps)(SearchPage);
+export default SearchPage;
