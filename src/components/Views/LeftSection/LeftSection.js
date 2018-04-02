cde9970394d29b5705b7157bd350828b510bf027
Anshul2166 <anshuldubey2166@gmail.com>
2018-04-02 14:25:01 +0530

    Refracted checkbox and topoffers and deals

--- src/components/Views/LeftSection/LeftSection.js
+++ src/components/Views/LeftSection/LeftSection.js
@@ -14,9 +14,9 @@ import "./LeftSection.css";
 
 class LeftSection extends Component {
     constructor(props) {
-      super(props);
-    
-      this.state = {};
+        super(props);
+
+        this.state = {};
     }
     render() {
         const { trendingItems, coupons, stores } = this.props;
@@ -24,9 +24,16 @@ class LeftSection extends Component {
             <div className="left-section">
                 <ImageLogo />
                 <p>50 offers available</p>
-                <CouponType titles={this.props.json.checkBoxTitle}/>
-                <About description={this.props.json.aboutUs}/>
-                <Stats stats={this.props.json.stats}/>
+                <CouponType titles={this.props.json.checkBoxTitle} />
+                <About
+                    titles={this.props.json.title}
+                    description={this.props.json.aboutUs}
+                />
+                <TopOffers
+                    titles={this.props.json.title}
+                    deals={this.props.json.todaysDeal}
+                />
+                <Stats stats={this.props.json.stats} />
             </div>
         );
     }
@@ -38,51 +45,48 @@ const ImageLogo = props => (
     </div>
 );
 
-function CheckBoxCoupon(props){
-    return(
+function CheckBoxCoupon(props) {
+    return (
         <Checkbox className="input-checkbox-label">
             <label className="checkbox-title">{props.title}</label>
         </Checkbox>
     );
 }
 
-function CheckBoxCouponList(props){
-    const ListItems=props.titles.map((title)=>
-        <CheckBoxCoupon title={title.toString()} />
-    );
-    return(
-        <div>
-        {ListItems}
-        </div>
-    );
+function CheckBoxCouponList(props) {
+    const ListItems = props.titles.map(checkTitle => (
+        <CheckBoxCoupon title={checkTitle.title} />
+    ));
+    return <div>{ListItems}</div>;
 }
 
 const CouponType = props => (
     <div className="coupon-type">
         <h4>Coupon Type</h4>
-        <CheckBoxCouponList titles={props.titles}/>
+        <CheckBoxCouponList titles={props.titles} />
     </div>
 );
 
 const About = props => (
     <div className="about">
-        <h4>About Udemy</h4>
-        <p className="about-content">
-            {props.description}
-        </p>
-        <TopOffers />
+        <h4>About {props.titles}</h4>
+        <p className="about-content">{props.description}</p>
     </div>
 );
 
-const TopOffers = props => (
-    <div className="top-offers">
-        <h2>Today's Udemy Top Offers</h2>
-        <ul>
-            <li>3 Courses Or More for $9.99</li>
-            <li>95% Off Ethical Hacking With Android Fast-Track Course</li>
-        </ul>
-    </div>
-);
+function TopOffers(props){
+    const TopDealsList = props.deals.map(dealTitle => (
+        <li>{dealTitle.deal}</li>
+    ));
+    return (
+        <div className="top-offers">
+            <h2>Today's {props.titles} Top Offers</h2>
+            <ul>
+                {TopDealsList}
+            </ul>
+        </div>
+    );
+}
 
 const Stats = props => (
     <div className="stats">
