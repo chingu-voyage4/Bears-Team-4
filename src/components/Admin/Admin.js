9fb6a394d7618bdfdd15889fa0b16bfd3ec65ca4
Anshul2166 <anshuldubey2166@gmail.com>
2018-04-12 11:57:22 +0530

    Setup the admin

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,16 @@
+
+import { Route } from "react-router-dom";
+import React, { Component } from "react";
+
+import "./Admin.css";
+
+class Admin extends Component {
+  render() {
+    return (
+      <div className="Admin">
+      </div>
+    );
+  }
+}
+
+export default Admin;
