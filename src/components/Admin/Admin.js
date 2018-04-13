2fc4769aa1748443399a5cc0b74a58c82df454d7
Anshul2166 <anshuldubey2166@gmail.com>
2018-04-13 12:00:12 +0530

    Coupon showing in admin page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,27 +1,51 @@
-
 import { Route } from "react-router-dom";
 import React, { Component } from "react";
-import {Row, Col } from "react-bootstrap";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Row, Col } from "react-bootstrap";
+import TopOffers from "../MainPage/TopOffers/TopOffers";
+import * as couponActions from "../../actions/couponActions";
+import * as userActions from "../../actions/userActions";
 import "./Admin.css";
 
 class Admin extends Component {
-  render() {
-    return (
-      <div className="Admin">
-      	<Row>
-      		<h2 className="text-center">Coupon Validation Space</h2>
-      	</Row>
-    	<Row>
-    		<Col sm={2}>
-
-    		</Col>
-    		<Col sm={8}>
-
-    		</Col>
-    	</Row>
-      </div>
-    );
-  }
+	componentDidMount() {
+		this.props.couponActions.FetchTrendingAndTopOffers();
+		this.props.userActions.GetUserLocation();
+	}
+	render() {
+		const { coupons } = this.props;
+		return (
+			<div className="Admin">
+				<Row>
+					<h2 className="text-center">Coupon Validation Space</h2>
+				</Row>
+				<Row>
+					<Col sm={2} />
+					<Col sm={8}>
+						<TopOffers coupons={coupons} />
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
 
-export default Admin;
+// Specifying which state from store we want as props
+const mapStateToProps = state => {
+	return {
+		stores: state.stores,
+		coupons: state.coupons
+	};
+};
+
+// Specifying which actions from store we want as props
+const mapActionsToProps = dispatch => {
+	return {
+		couponActions: bindActionCreators(couponActions, dispatch),
+		userActions: bindActionCreators(userActions, dispatch)
+	};
+};
+
+// Binding stores states and actions to our compoent's props
+export default connect(mapStateToProps, mapActionsToProps)(Admin);
