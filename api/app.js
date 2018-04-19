172134f429dcdf6194b81a6467d2f6cc81aeed82
Sandun <sandunwebdev@gmail.com>
2018-04-19 08:30:46 +0530

    Fixed Heroku Port Problem

--- api/app.js
+++ api/app.js
@@ -1,5 +1,12 @@
-const app = require('./server');
-// listen for the requests
-app.listen(8080, function() {
-    console.log('CORS-enabled web server listening on port 8080');
-  });
+const app = require("./server");
+
+// Set Variable called port according to "development" / "production"
+if (process.env.NODE_ENV === "production") {
+  app.set("port", process.env.PORT || 80);
+} else {
+  app.set("port", 8080);
+}
+
+app.listen(app.get("port"), () => {
+  console.log(`Listening on ${app.get("port")}`);
+});
